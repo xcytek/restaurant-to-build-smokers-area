386f09b48bb17fc1a3890020a7f04c8bf3109874
Alex Acosta <alexcytek@gmail.com>
2019-12-08 01:04:37 -0800

    more changes

--- DataMiningRestaurants.R
+++ DataMiningRestaurants.R
@@ -6,17 +6,13 @@
 # if they should build a smokers area in their facilities
 
 ##### Data Importing #####
-setwd("smokers-area/")
+setwd("~/Documents/Maestria/2 Cuatrimestre/Analitica de Negocio/Unidad 4/uci-restaurant-consumer-data/smokers-area/")
 user_profile <- read.table("userprofile.csv", header = TRUE, sep = ",")
-
 
 ##### Data Preprocessing #####
 library(dplyr)
-str(user_profile)
-summary(user_profile)
 
 user_profile$smoker[user_profile$smoker == "?"] <- NA
-user_profile$drink_level[user_profile$drink_level == "?"] <- NA
 
 # Only customers from 1950 and 2000 (People actually pays)
 users_can_pay <- user_profile %>% 
@@ -25,29 +21,54 @@ users_can_pay <- user_profile %>%
   # Only people are likely to pay
   filter(birth_year >= 1980 & birth_year <= 2000) 
 
+# Data summary
+str(users_can_pay)
+summary(users_can_pay)
 
 ##### Data visualization #####
 library(ggplot2)
 
-# Distribution of Height and Weight of the original customers
+# 1) Distribution of Height of the original customers
 ggplot(data = user_profile, aes(x = height)) + 
   geom_histogram(bins = 40, fill = "lightblue", col = "blue")
 
+# 2) Distribution of Weight of the original customers
 ggplot(data = user_profile, aes(x = weight)) + 
   geom_histogram(bins = 40, fill = "lightblue", col = "blue")
 
-# Distribution customers can actually pay
+# 3) Distribution customers can actually pay based on their age
 ggplot(data = users_can_pay, aes(x = birth_year)) + 
   geom_histogram(bins = 10, fill = "lightblue", col = "blue")
+
+
+# Relation between customer's birth year and weight
+ggplot(data = na.omit(user_profile), aes(x = weight, y = birth_year)) +
+  geom_point(mapping = aes(color = smoker)) +
+  geom_smooth() 
+
+# Relation between customer's birth year and weight by smokers separately
+ggplot(data = na.omit(user_profile), aes(x = weight, y = birth_year)) +
+  geom_point(mapping = aes(color = smoker)) +
+  geom_smooth() +
+  facet_wrap(~ smoker, nrow = 1)
+
+# Relation between customer's birth year and height
+ggplot(data = na.omit(user_profile), aes(x = height, y = birth_year)) +
+  geom_point(mapping = aes(color = smoker)) +
+  geom_smooth()
+
+# Relation between customer's birth year and height by smokers separately
+ggplot(data = na.omit(user_profile), aes(x = height, y = birth_year)) +
+  geom_point(mapping = aes(color = smoker)) +
+  geom_smooth() +
+  facet_wrap(~ smoker, nrow = 1)
 
 
 ##### Data prediction #####
 library(caTools)
 
 # Creating Train and Test data
-BirthYear <- c()
-Weight    <- c()
-Height    <- c()
+BirthYear <- c() ; Weight <- c() ; Height <- c()
 
 BirthYear$split <- sample.split(users_can_pay$birth_year, SplitRatio = 0.65)
 BirthYear$train <- subset(users_can_pay, BirthYear$split == TRUE)
@@ -65,7 +86,7 @@ Height$test  <- subset(users_can_pay, Height$split == FALSE)
 # Model building for Birth Year
 BirthYear$model  <- lm(data = BirthYear$train, birth_year~.)
 BirthYear$result <- predict(BirthYear$model, BirthYear$test)
-BirthYear$compare <- as.data.frame(cbind(actual = BirthYear$test$birth_year, predicted = as.integer(BirthYear$result1)))
+BirthYear$compare <- as.data.frame(cbind(actual = BirthYear$test$birth_year, predicted = as.integer(BirthYear$result)))
 BirthYear$error   <- BirthYear$compare$actual - BirthYear$compare$predicted
 cbind(BirthYear$compare, BirthYear$error)
 BirthYear$rmse <- sqrt(mean(BirthYear$error ^ 2, na.rm = TRUE))
@@ -91,25 +112,35 @@ Height$rmse
 
 
 ### Boxplot relation between customers who are smoker by birth year
+library(gridExtra)
 
 # Original Data
-ggplot(data = na.omit(users_can_pay), aes(y = birth_year, x = smoker, fill = smoker)) +
+BirthYear$boxplot <- ggplot(data = na.omit(users_can_pay), aes(y = birth_year, x = smoker, fill = smoker)) +
+  ggtitle("Original") +
   geom_boxplot()
 
-# Train Data
-ggplot(data = na.omit(BirthYear$train), aes(y = birth_year, x = smoker, fill = smoker)) +
+# Test Data
+BirthYear$boxplot_test <- ggplot(data = na.omit(BirthYear$test), aes(y = birth_year, x = smoker, fill = smoker)) +
+  ggtitle("Test") +
   geom_boxplot()
+
+# Plot both
+grid.arrange(BirthYear$boxplot, BirthYear$boxplot_test, nrow = 1)
 
 ### Boxplot relation between customers who are smoker by weight
 
 # Original Data
-ggplot(data = na.omit(users_can_pay), aes(y = weight, x = smoker, fill = smoker)) +
+Weight$boxplot <- ggplot(data = na.omit(users_can_pay), aes(y = weight, x = smoker, fill = smoker)) +
+  ggtitle("Original") +
   geom_boxplot()
 
-# Train Data
-ggplot(data = na.omit(Weight$test), aes(y = weight, x = smoker, fill = smoker)) +
+# Test Data
+Weight$boxplot_test <- ggplot(data = na.omit(Weight$test), aes(y = weight, x = smoker, fill = smoker)) +
+  ggtitle("Original") +
   geom_boxplot()
 
+# Plot both 
+grid.arrange(Weight$boxplot, Weight$boxplot_test, nrow = 1)
 
 ### Boxplot relation between customers who are smoker by height
 
